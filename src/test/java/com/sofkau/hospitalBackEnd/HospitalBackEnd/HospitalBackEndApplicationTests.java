1203b953a6c174302c0b750aafc86b4d63e1c84b
Drownfe <palaciojuanfe@gmail.com>
2022-08-03 15:20:32 -0500

    Project created, Hospital V2

---
+++ src/test/java/com/sofkau/hospitalBackEnd/HospitalBackEnd/HospitalBackEndApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sofkau.hospitalBackEnd.HospitalBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalBackEndApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
